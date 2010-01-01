a2191fdfb52b26bf98155250f6dd9ec379652519
joshn53 <joshn53@05c81399-d40f-49fa-a30b-2157e65eb73f>
2010-01-01 18:51:46 +0000

    Add files that should have been included in previous commit.
    git-svn-id: https://vcs.maemo.org/svn/katana@18 05c81399-d40f-49fa-a30b-2157e65eb73f

---
+++ trunk/search_results.cpp
@@ -0,0 +1,89 @@
+#include "search_results.h"
+
+#include "bible_text_source.h"
+
+#include <iostream>
+
+#include <Qt/qmaemo5kineticscroller.h>
+#include <QPushButton>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QSignalMapper>
+#include <QVBoxLayout>
+
+SearchResultsFrame::SearchResultsFrame()
+{
+	mLayout = new QVBoxLayout;
+	mLayout->setSpacing(2);
+	mLayout->setContentsMargins(0, 0, 0, 0);
+	setLayout(mLayout);
+	mSearchResultsMapper = NULL;
+
+	//setStyleSheet("QFrame{background: #000;}");
+}
+
+SearchResultsFrame::~SearchResultsFrame()
+{
+}
+
+void SearchResultsFrame::handleResults(QList<Key> results)
+{
+	hideResults();
+	mLayout->setContentsMargins(3, 3, 0, 3);
+
+	mSearchResultsMapper = new QSignalMapper();
+	connect(mSearchResultsMapper, SIGNAL(mapped(const QString&)), this,
+			SLOT(onSelect(const QString&)));
+
+	QVBoxLayout* buttonsLayout = new QVBoxLayout;
+	buttonsLayout->setSpacing(0);
+	buttonsLayout->setContentsMargins(0, 0, 0, 0);
+	for (int i = 0; i < results.count(); i++)
+	{
+		Key key = results[i];
+		QString result = QString("%1 %2:%3").arg(key.mBook)
+											.arg(key.mChapter+1)
+											.arg(key.mVerse+1);
+		QPushButton* button = new QPushButton(result);
+		QFont font = button->font();
+		font.setPointSize(12);
+		button->setFont(font);
+		mSearchResultsMapper->setMapping(button, result);
+		connect(button, SIGNAL(clicked()), mSearchResultsMapper, SLOT(map()));
+		buttonsLayout->addWidget(button);
+	}
+	mScroll = new QScrollArea;
+	mScroll->setFrameShape(QFrame::NoFrame);
+	mScroll->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	mScroll->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	mScroll->setSizePolicy(QSizePolicy(QSizePolicy::Maximum,
+									QSizePolicy::Preferred));
+	mScroller = new QMaemo5KineticScroller(mScroll);
+
+	QFrame* frame = new QFrame();
+	frame->setStyleSheet("QPushButton{"
+						"padding: 3px 10px 3px 10px;"
+						"}");
+	frame->setLayout(buttonsLayout);
+	mScroll->setWidget(frame);
+	mLayout->addWidget(mScroll);
+}
+
+void SearchResultsFrame::hideResults()
+{
+	mLayout->setContentsMargins(0, 0, 0, 0);
+	while (mLayout->count() > 0)
+	{
+		QLayoutItem* item = mLayout->itemAt(0);
+		mLayout->removeItem(item);
+		delete item;
+	}
+	delete mSearchResultsMapper;
+	mSearchResultsMapper = NULL;
+}
+
+void SearchResultsFrame::onSelect(const QString& result)
+{
+	emit resultSelected(result);
+}
+
---
+++ trunk/search_results.h
@@ -0,0 +1,31 @@
+#include <QFrame>
+
+class Key;
+class QVBoxLayout;
+class QPushButton;
+class QScrollArea;
+class QSignalMapper;
+class QMaemo5KineticScroller;
+class SearchResultsFrame : public QFrame
+{
+	Q_OBJECT
+
+public:
+	SearchResultsFrame();
+	~SearchResultsFrame();
+
+	void handleResults(QList<Key> results);
+	void hideResults();
+
+signals:
+	void resultSelected(const QString& result);
+
+protected slots:
+	void onSelect(const QString& result);
+
+protected:
+	QVBoxLayout* mLayout;
+	QSignalMapper* mSearchResultsMapper;
+	QScrollArea* mScroll;
+	QMaemo5KineticScroller* mScroller;
+};
