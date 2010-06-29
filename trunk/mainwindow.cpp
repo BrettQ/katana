571fe483c06f7f2673cacc2e513eea58dfecd75d
joshn53 <joshn53@05c81399-d40f-49fa-a30b-2157e65eb73f>
2010-06-29 02:55:26 +0000

    Use "Go to Passage" rather than "Go to Chapter".
    git-svn-id: https://vcs.maemo.org/svn/katana@66 05c81399-d40f-49fa-a30b-2157e65eb73f

--- trunk/mainwindow.cpp
+++ trunk/mainwindow.cpp
@@ -88,7 +88,7 @@ void MainWindow::closeEvent(QCloseEvent* event)
 
 void MainWindow::createMenu()
 {
-	menuBar()->addAction("Go to Chapter", this, SLOT(onSelectVerse()));
+	menuBar()->addAction("Go to Passage", this, SLOT(onSelectVerse()));
 	menuBar()->addAction("Select Translation", this,
 						SLOT(selectTranslation()));
 	menuBar()->addAction("Search", this, SLOT(onSearch()));
